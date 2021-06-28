d144ba947cd77f53c58f1e15e8853ac5ad294468
alamgir hossain <alamgir555hossain@gmail.com>
2021-06-27 22:18:36 -0400

    bug-fix

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
-  title: { type: String, require: true, maxlength: 128 },
-  company: {type: String, require: true, maxlength: 64},
-  companyWebsite: {type: String, require: true, maxlength: 128},
-  location: {type: String, require: true},
-  jobTitle: {type: String, require: true},
-  description: { type: String, require: true },
-  startDate: {type: Date, require: true},
+  title: { type: String, required: true, maxlength: 128 },
+  company: {type: String, required: true, maxlength: 64},
+  companyWebsite: {type: String, required: true, maxlength: 128},
+  location: {type: String, required: true},
+  jobTitle: {type: String, required: true},
+  description: { type: String, required: true },
+  startDate: {type: Date, required: true},
   endDate: {type: Date},
-  userId: {type: String, require: true},
+  userId: {type: String, required: true},
   createdAt: {type: Date, default: Date.now}
 });
 
